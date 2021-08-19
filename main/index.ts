359d9b9b8af842c8a92d9072a49c44da5f874862
Alendia <alendia0807@gmail.com>
2021-08-19 23:28:37 +0800

    feat: first commit

---
+++ main/index.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile(path.resolve(app.getAppPath(), "dist/index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
